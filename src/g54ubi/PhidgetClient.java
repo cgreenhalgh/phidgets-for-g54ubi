2ac53c58ef73af0eb75f95a96b8e7b73bd16d9ce
Chris Greenhalgh <cmg@cs.nott.ac.uk>
2013-02-25 12:33:28 +0000

    initial version with property-based config for ifkit

--- src/g54ubi/PhidgetClient.java
+++ src/g54ubi/PhidgetClient.java
@@ -3,8 +3,14 @@
  */
 package g54ubi;
 
+import java.io.FileInputStream;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.Vector;
 
 import com.phidgets.*;
 import com.phidgets.event.*;
@@ -30,13 +36,20 @@ public class PhidgetClient {
 				synchronized (this) {
 					while(!done) {
 						int count = 0;
-						for (Phidget phid : phidgets.values()) {
+						Collection<Phidget> phidgets2 = null;
+						synchronized (phidgets) {
+							// clone list to avoid concurrent update problems
+							phidgets2 = new Vector<Phidget>(phidgets.values());
+						}
+						for (Phidget phid : phidgets2) {
 							if (phid instanceof RFIDPhidget) {
 								RFIDPhidget rfid = (RFIDPhidget)phid;
 								try {
 									if (rfid.isAttached()) {
 										rfid.setAntennaOn(true);
 										rfid.setLEDOn(true);
+										// There doesn't seem to be a specific minimum to have the antenna on
+										// but 20ms seems very unreliable; 50ms a bit unreliable; 100ms fairly reliable.
 										wait(100);
 										count++;
 										String tag = rfid.getLastTag();
@@ -70,18 +83,251 @@ public class PhidgetClient {
 		}
 	}
 	
+	/** a value to be published, including sensor value scaling logic */
+	static class Value {
+		private String id;
+		private String name;
+		private String value;
+		private int ivalue;
+		private double dvalue;
+		private boolean changed;
+		private long changedTime;
+		private double scale = 1.0;
+		private double offset;
+		
+		Value(String id, String name, String initialValue) {
+			this.id = id;
+			this.name = name;
+			this.value = initialValue;
+			if (initialValue!=null) {
+				this.changed = true;
+				this.changedTime = System.currentTimeMillis();
+			}
+		}
+		
+		public Value(String id, String sensorname, double scale, double offset, int value) {
+			this.id = id;
+			this.name = sensorname;
+			this.scale = scale;
+			this.offset = offset;
+			setValue(value);
+		}
+
+		synchronized void setValue(String value) {
+			if (value!=null) {
+				if (!value.equals(this.value)) {
+					this.value = value;
+					this.changed = true;
+					this.changedTime = System.currentTimeMillis();
+					System.out.println("Value "+name+" = "+value);
+					this.notifyAll();
+				}
+			}
+		}
+
+		synchronized public void setValue(int value) {
+			if (value!=this.ivalue) {
+				ivalue = value;
+				dvalue = value*scale+offset;
+				this.value = Double.toString(dvalue);
+				changed = true;
+				changedTime = System.currentTimeMillis();
+				System.out.println("Value "+name+" = "+this.value);
+				this.notifyAll();
+			}
+		}
+	}
+	/** all current known values */
+	static Map<String,Value> values = new HashMap<String,Value>();
+	/** all current know value ids; NB not updated, replaced! */
+	static Set<String> valueIds = new HashSet<String>();
+
+	private static synchronized void insertValue(Value val) {
+		values.put(val.id, val);
+		// copy key set for thread safety
+		valueIds = new HashSet<String>(values.keySet());				
+	}
+
+	
+	private static final String PROPERTIES_FILE = "phidget.properties";
+	private static final String PHIDGET_SERVER = "phidget.server";
+	private static final String ID_SUFFIX = ".id";
+	private static final String NAME_SUFFIX = ".name";
+	private static final String IFKIT_PREFIX = "ifkit.";
+	protected static final String SENSOR_INFIX = ".sensor.";
+	protected static final String SCALE_SUFFIX = ".scale";
+	protected static final String OFFSET_SUFFIX = ".offset";
+	
+	/** get config helper */
+	static String getProperty(Properties props, String key, String defaultValue) {
+		String value = props.getProperty(key);
+		if (value==null) {
+			System.out.println("Property "+key+" = "+defaultValue+ " (default)");
+			return defaultValue;
+		}
+		System.out.println("Property "+key+" = "+value);
+		return value;		
+	}
+	/** get config helper */
+	static int getProperty(Properties props, String key, int defaultValue) {
+		String svalue = props.getProperty(key);
+		if (svalue==null) {
+			System.out.println("Property "+key+" = "+defaultValue+ " (default)");
+			return defaultValue;
+		}
+		try {
+			int value = Integer.parseInt(svalue);
+			System.out.println("Property "+key+" = "+value);
+			return value;				
+		}
+		catch (NumberFormatException nfe) {
+			System.out.println("Property "+key+" = "+defaultValue+" (Invalid non-int value: "+svalue+")");
+			return defaultValue;
+		}
+	}
+	/** get config helper */
+	static double getProperty(Properties props, String key, double defaultValue) {
+		String svalue = props.getProperty(key);
+		if (svalue==null) {
+			System.out.println("Property "+key+" = "+defaultValue+ " (default)");
+			return defaultValue;
+		}
+		try {
+			double value = Double.parseDouble(svalue);
+			System.out.println("Property "+key+" = "+value);
+			return value;				
+		}
+		catch (NumberFormatException nfe) {
+			System.out.println("Property "+key+" = "+defaultValue+" (Invalid non-double value: "+svalue+")");
+			return defaultValue;
+		}
+	}
+	
+	/** get config helper - look for <prefix><i>.id = <id> with i=0.. */
+	static String findPropertiesKey(String prefix, String id) {
+		int defaulti = -1;
+		for(int i=0; true; i++) {
+			String idkey = prefix+i+ID_SUFFIX;
+			String idvalue = props.getProperty(idkey);
+			String namekey = prefix+i+NAME_SUFFIX;
+			String namevalue = props.getProperty(namekey);
+			if (idvalue==null && namevalue!=null) {
+				defaulti = i;
+			}
+			else if (idvalue!=null && idvalue.equals(id))
+				return prefix+i;
+			else if (namevalue==null)
+				break;
+		}
+		if (defaulti>=0) {
+			System.out.println("Note: using default configuration "+defaulti+" for ID "+id);
+			return prefix+defaulti;
+		}
+		System.out.println("Warning: no configuration found for "+prefix+" for ID "+id);
+		return prefix+0;
+	}
+	
+	static void handleAttachInterfaceKit(String serverID, final int id) throws PhidgetException {
+		// TODO Auto-generated method stub
+		final InterfaceKitPhidget ifkit = new InterfaceKitPhidget();
+		if (serverID!=null)											
+			ifkit.open(id, serverID);
+		else
+			ifkit.open(id);
+		synchronized(phidgets) {
+			phidgets.put(id, ifkit);
+		}
+		// find in properties...
+		final String prefix = findPropertiesKey(IFKIT_PREFIX, String.valueOf(id));
+		final String ifkitname = getProperty(props, prefix+NAME_SUFFIX, "undefined");
+
+		System.out.println("Added InterfaceKit "+id+ "("+ifkitname+")");
+		
+		ifkit.addSensorChangeListener(new SensorChangeListener() {
+			
+			@Override
+			public void sensorChanged(SensorChangeEvent sce) {
+				// TODO Auto-generated method stub
+				System.out.println("Sensor change "+sce.getIndex()+" = "+sce.getValue());
+				String sensorid = IFKIT_PREFIX+id+SENSOR_INFIX+sce.getIndex();
+				Value val = values.get(sensorid);
+				if (val!=null) {
+					val.setValue(sce.getValue());
+				}
+				else {
+					System.out.println("Error: sensor change for unknown sensor "+sensorid);
+				}
+			}
+		});
+		ifkit.addAttachListener(new AttachListener() {
+			
+			@Override
+			public void attached(AttachEvent ae) {
+				// TODO Auto-generated method stub
+				System.out.println("interfacekit "+id+" attached");
+				int sensors;
+				try {
+					sensors = ifkit.getSensorCount();
+					System.out.println("interfacekit reports "+sensors+" sensors");
+					if (sensors>8)
+						sensors = 8;
+					for (int i=0; i<sensors; i++) {
+						String sensorid = IFKIT_PREFIX+id+SENSOR_INFIX+i;
+						
+						int value = ifkit.getSensorValue(i);
+						System.out.println("Sensor initial "+i+" = "+ifkit.getSensorValue(i));
+						
+						String sensorprefix = prefix+SENSOR_INFIX+i;
+						String sensorname = getProperty(props, sensorprefix+NAME_SUFFIX, "undefined"+i);
+						// TODO name prefix
+						double scale = getProperty(props, sensorprefix+SCALE_SUFFIX, 1.0);
+						double offset = getProperty(props, sensorprefix+OFFSET_SUFFIX, 0.0);
+						Value val = values.get(sensorid);
+						if (val==null){
+							val = new Value(sensorid, sensorname, scale, offset, value);
+							insertValue(val);
+						}
+						else 
+							val.setValue(value);
+					}
+				} catch (PhidgetException e) {
+					// TODO Auto-generated catch block
+					System.err.println("Error reading ifkit:" +e);
+					e.printStackTrace(System.err);
+				}
+			}
+
+		});
+		ifkit.addDetachListener(new DetachListener() {
+			
+			@Override
+			public void detached(DetachEvent arg0) {
+				// TODO Auto-generated method stub
+				System.out.println("interfacekit "+id+" detached");
+			}
+		});
+		
+	}
+
+	static Properties props = new Properties();
+	static Map<Integer,Phidget> phidgets = new HashMap<Integer,Phidget>();
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			final String serverID = (args.length>0 ? args[0] : null); //"mrlphidgetsbc1";
+			System.out.println("Reading properties from "+PROPERTIES_FILE);
+			//Properties props = new Properties();
+			props.load(new FileInputStream(PROPERTIES_FILE));
+			
+			String serverProp = getProperty(props, PHIDGET_SERVER, null);
+			final String serverID = (args.length>0 ? args[0] : serverProp); //"mrlphidgetsbc1";
+			System.out.println("using phidget.server "+serverID);
 			
 			Manager man;
 			System.out.println(Phidget.getLibraryVersion());
 
-			final Map<Integer,Phidget> phidgets = new HashMap<Integer,Phidget>();
 			
 			man = new Manager();
 			man.addAttachListener(new AttachListener()
@@ -99,7 +345,7 @@ public class PhidgetClient {
 									rfid.open(id, serverID);
 								else
 									rfid.open(id);
-								phidgets.put(ae.getSource().getSerialNumber(), rfid);
+								// delay add to attached
 								if (false) {
 									// use polling instead
 									rfid.addTagGainListener(new TagGainListener() {
@@ -126,6 +372,9 @@ public class PhidgetClient {
 									public void attached(AttachEvent ae) {
 										// TODO Auto-generated method stub
 										System.out.println("RFID "+id+" attached");
+										synchronized(phidgets) {
+											phidgets.put(id, rfid);
+										}
 										try {
 											rfid.setAntennaOn(false);
 											rfid.setLEDOn(false);
@@ -145,52 +394,7 @@ public class PhidgetClient {
 								});
 							}
 							else if (phid.getDeviceClass()==Phidget.PHIDCLASS_INTERFACEKIT) {
-								System.out.println("Added InterfaceKit "+id);
-								final InterfaceKitPhidget ifkit = new InterfaceKitPhidget();
-								if (serverID!=null)											
-									ifkit.open(id, serverID);
-								else
-									ifkit.open(id);
-								phidgets.put(ae.getSource().getSerialNumber(), ifkit);
-								ifkit.addSensorChangeListener(new SensorChangeListener() {
-									
-									@Override
-									public void sensorChanged(SensorChangeEvent sce) {
-										// TODO Auto-generated method stub
-										System.out.println("Sensor change "+sce.getIndex()+" = "+sce.getValue());
-									}
-								});
-								ifkit.addAttachListener(new AttachListener() {
-									
-									@Override
-									public void attached(AttachEvent ae) {
-										// TODO Auto-generated method stub
-										System.out.println("interfacekit "+id+" attached");
-										int sensors;
-										try {
-											sensors = ifkit.getSensorCount();
-											System.out.println("interfacekit reports "+sensors+" sensors");
-											if (sensors>8)
-												sensors = 8;
-											for (int i=0; i<sensors; i++) {
-												int value = ifkit.getSensorValue(i);
-												System.out.println("Sensor initial "+i+" = "+ifkit.getSensorValue(i));
-											}
-										} catch (PhidgetException e) {
-											// TODO Auto-generated catch block
-											System.err.println("Error reading ifkit:" +e);
-											e.printStackTrace(System.err);
-										}
-									}
-								});
-								ifkit.addDetachListener(new DetachListener() {
-									
-									@Override
-									public void detached(DetachEvent arg0) {
-										// TODO Auto-generated method stub
-										System.out.println("interfacekit "+id+" detached");
-									}
-								});
+								handleAttachInterfaceKit(serverID, id);
 							}
 						}
 					} catch (PhidgetException e) {
@@ -200,6 +404,7 @@ public class PhidgetClient {
 					}
 					
 				}
+
 			});
 			man.addDetachListener(new DetachListener()
 			{
@@ -209,7 +414,10 @@ public class PhidgetClient {
 			});
 			try
 			{
-				man.open("mrlphidgetsbc1");
+				if (serverID!=null)
+					man.open(serverID);
+				else
+					man.open();
 			}
 			catch (Exception e)
 			{

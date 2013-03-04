d11331fb7fde3d45382a89b1d219c4843e5b1091
Chris Greenhalgh <cmg@cs.nott.ac.uk>
2013-03-04 10:32:42 +0000

    added script prefix

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 javac -cp lib/log4j.jar:lib:lib/mariner.jar:lib/phidget21.jar:lib/union.jar -d bin src/g54ubi/*.java
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 java -cp lib/log4j.jar:lib:lib/mariner.jar:lib/phidget21.jar:lib/union.jar:bin:. g54ubi.UnionClient > tmp.txt 2>&1 &
